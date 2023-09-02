43cfae075645c775d6aebc3a8de85b9d41fdfe9c
Topitops69 <topitops123@gmail.com>
2023-09-03 01:42:37 +0800

    update

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,25 @@
+"use client";
+
+import { ArrowRight } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+import { cn } from "@/lib/utils";
 
 
-export default function DashboardPage() {
-  return(
-    <div>
-        <p>Dashboard Page (Protected)</p>
-    </div>
 
-  )
+export default function HomePage() {
+  const router = useRouter();
+
+  return (
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          Explore the power of AI
+        </h2>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+          Chat with the smartest AI - Experience the power of AI
+        </p>
+        
+      </div>
+      
+  );
 }
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,8 +3,17 @@ import {Montserrat} from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
 
-import { cn } from "@/lib/utils";
-import { LayoutDashboard, MessageSquare, ImageIcon, VideoIcon, MusicIcon, Code, Settings} from "lucide-react";
+import {cn} from "@/lib/utils";
+import {
+    LayoutDashboard,
+    MessageSquare,
+    ImageIcon,
+    VideoIcon,
+    MusicIcon,
+    Code,
+    Settings
+} from "lucide-react";
+import {usePathname} from "next/navigation";
 
 const montserrat = Montserrat({weight: "600", subsets: ["latin"]});
 
@@ -13,47 +22,42 @@ const routes = [
         label: "Dashboard",
         icon: LayoutDashboard,
         href: "/dashboard",
-        color: "text-sky-500",
-    },
-    {
+        color: "text-sky-500"
+    }, {
         label: "Conversation",
         icon: MessageSquare,
-        href: "/dashboard",
-        color: "text-violet-500",
-    },
-    {
+        href: "/conversation",
+        color: "text-violet-500"
+    }, {
         label: "Image Generation",
         icon: ImageIcon,
-        href: "/dashboard",
-        color: "text-pink-500",
-    },
-    {
+        href: "/image",
+        color: "text-pink-500"
+    }, {
         label: "Video Generation",
         icon: VideoIcon,
-        href: "/dashboard",
-        color: "text-orange-700",
-    },
-    {
+        href: "/video",
+        color: "text-orange-700"
+    }, {
         label: "Music Generation",
         icon: MusicIcon,
-        href: "/dashboard",
-        color: "text-emerald-500",
-    },
-    {
+        href: "/music",
+        color: "text-emerald-500"
+    }, {
         label: "Code Generation",
         icon: Code,
-        href: "/dashboard",
-        color: "text-green-700",
-    },
-    {
+        href: "/code",
+        color: "text-green-700"
+    }, {
         label: "Settings",
         icon: Settings,
-        href: "/dashboard",
-        
-    },
+        href: "/settings"
+    }
 ]
 
 const Sidebar = () => {
+    const pathname = usePathname();
+
     return (
         <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
             <div className="px-3 py-2 flex-1">
@@ -61,20 +65,21 @@ const Sidebar = () => {
                     <div className="relative w-8 h-8 mr-4">
                         <Image fill alt="Logo" src="/logo.png"/>
                     </div>
-                    <h1 className={cn ("text-2xt font-bold", montserrat.className)}>
+                    <h1 className={cn("text-2xt font-bold", montserrat.className)}>
                         GiGaChat
                     </h1>
                 </Link>
                 <div className="space-y-1">
-                    {routes.map( (route) => (
-                        <Link  
+                    {routes.map((route) => (
+                        <Link
                             href={route.href}
                             key={route.href}
-                            className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
-                        >
+                            //highlight
+                            className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                                pathname === route.href ? "text-white bg-white/10" : "text-zinc"
+                            )}>
                             <div className="flex items-center flex-1">
-                                <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
-                                {route.label}
+                                <route.icon className={cn("h-5 w-5 mr-3", route.color)}/> {route.label}
                             </div>
                         </Link>
                     ))}
