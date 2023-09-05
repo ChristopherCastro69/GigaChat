be27217b721bfa22ebad2f1736800ee4a01d804c
Topitops69 <topitops123@gmail.com>
2023-09-05 22:08:57 +0800

    completed the code generation and the conversation mode

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -63,7 +63,11 @@ const Sidebar = () => {
             <div className="px-3 py-2 flex-1">
                 <Link href="/dashboard" className="flex items-center pl-3 mb-14">
                     <div className="relative w-8 h-8 mr-4">
-                        <Image fill alt="Logo" src="/logo.png"/>
+                        <Image 
+                        fill alt="Logo" 
+                        src="/logo.png" 
+                        sizes="(max-width: 640px) 100vw, (max-width: 768px) 90vw, 80vw"
+                        />
                     </div>
                     <h1 className={cn("text-2xt font-bold", montserrat.className)}>
                         GiGaChat
@@ -71,13 +75,12 @@ const Sidebar = () => {
                 </Link>
                 <div className="space-y-1">
                     {routes.map((route) => (
-                        <Link
-                            href={route.href}
-                            key={route.href}
-                            //highlight
-                            className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
-                                pathname === route.href ? "text-white bg-white/10" : "text-zinc"
-                            )}>
+                        <Link href={route.href} key={route.href} //highlight
+    className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:tex" +
+                                "t-white hover:bg-white/10 rounded-lg transition",
+                        pathname === route.href
+                            ? "text-white bg-white/10"
+                            : "text-zinc")}>
                             <div className="flex items-center flex-1">
                                 <route.icon className={cn("h-5 w-5 mr-3", route.color)}/> {route.label}
                             </div>
